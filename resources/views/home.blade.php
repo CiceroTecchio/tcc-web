9223c8dd287f59c1c026dca72ac0dc76db6eb126
Cicero Tecchio <cicero_tecchio@hotmail.com>
2020-09-22 18:22:06 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -170,16 +170,16 @@
                 function() {
                     handleLocationError(true, infoWindow, map.getCenter());
                 });
-            navigator.geolocation.watchPosition(function(position) {
-                    var pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-                    userMarker.setPosition(new google.maps.LatLng(pos));
-                },
-                function() {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                });
+            // navigator.geolocation.watchPosition(function(position) {
+            //         var pos = {
+            //             lat: position.coords.latitude,
+            //             lng: position.coords.longitude
+            //         };
+            //         userMarker.setPosition(new google.maps.LatLng(pos));
+            //     },
+            //     function() {
+            //         handleLocationError(true, infoWindow, map.getCenter());
+            //     });
         } else {
             // Browser doesn't support Geolocation
             handleLocationError(false, infoWindow, map.getCenter());
